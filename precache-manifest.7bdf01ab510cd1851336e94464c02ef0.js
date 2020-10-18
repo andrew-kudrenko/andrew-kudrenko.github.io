5eb1d153945a099a32f7011313ed0a1705d12e8e
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2020-10-18 12:21:27 +0300

    Updates

---
+++ precache-manifest.7bdf01ab510cd1851336e94464c02ef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a661aaddf9aea60c06253bbe85638a07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fde8f1a515588e9844c1",
+    "url": "/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "8f043c073f4a00d9f818",
+    "url": "/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "fde8f1a515588e9844c1",
+    "url": "/static/js/2.64e3b8f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.64e3b8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f043c073f4a00d9f818",
+    "url": "/static/js/main.9e78289d.chunk.js"
+  },
+  {
+    "revision": "6dc55e8454f8e880e18d",
+    "url": "/static/js/runtime-main.b75280e8.js"
+  }
+]);
